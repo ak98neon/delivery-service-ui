d8f891739e3ff1c16eca1394ee5309dca941cffd
Artem Kudria <a.kudria@advcash.com>
2021-03-21 23:42:30 +0200

    Init Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {MainComponent} from './main/main.component';
+import { MainSidebarComponent } from './main/main-sidebar/main-sidebar.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        MainComponent
+        MainComponent,
+        MainSidebarComponent,
+        LoginComponent,
+        RegisterComponent,
+        AuthComponent
     ],
     imports: [
         BrowserModule,
