eee6de2adcbdb82a7bfd487eeb4c0c212570d0b2
Artem Kudria <a.kudria@advcash.com>
2021-03-25 11:03:30 +0200

    Finish order block

--- src/app/main/cab/order-list.component/order-list.component.ts
+++ src/app/main/cab/order-list.component/order-list.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+export interface Order {
+    name: string;
+    price: number;
+    count: number;
+    imgUrl: string;
+}
 
 @Component({
-  selector: 'app-order-list',
-  templateUrl: './order-list.component.html',
-  styleUrls: ['./order-list.component.scss']
+    selector: 'app-order-list',
+    templateUrl: './order-list.component.html',
+    styleUrls: ['./order-list.component.scss']
 })
 export class OrderListComponent implements OnInit {
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit(): void {
-  }
+    orderList: Order[] = [
+        {name: 'Mushroom Pizza', count: 1, price: 7.99, imgUrl: 'url(assets/img/transparent-pizza.png)'},
+        {name: 'Mushroom Pizza', count: 1, price: 7.99, imgUrl: 'url(assets/img/transparent-pizza.png)'},
+        {name: 'Mushroom Pizza', count: 1, price: 7.99, imgUrl: 'url(assets/img/transparent-pizza.png)'}
+    ];
+
+    ngOnInit(): void {
+    }
 
 }
