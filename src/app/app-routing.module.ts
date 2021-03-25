1a4e44c3ccb72cb2f119ddfc1b83284c2c2e37a8
Artem Kudria <a.kudria@advcash.com>
2021-03-25 12:47:58 +0200

    Add routing to home button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {LoginComponent} from './auth/login/login.component';
-import {AppComponent} from './app.component';
+import {MainComponent} from './main/main.component';
 
 const routes: Routes = [
     {path: 'login', component: LoginComponent},
-    {path: '', component: AppComponent},
-    {path: '**', component: AppComponent}
+    {path: '', component: MainComponent},
+    {path: '**', component: MainComponent}
 ];
 
 @NgModule({
