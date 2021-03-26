cf120dd0bc8a86f7e8370ceb463722eae245c6ec
Artem Kudria <a.kudria@advcash.com>
2021-03-26 23:43:46 +0200

    Add animated credit card

--- src/app/main/cab/user-card.component/user-card.component.ts
+++ src/app/main/cab/user-card.component/user-card.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-user-card',
-  templateUrl: './user-card.component.html',
-  styleUrls: ['./user-card.component.scss']
+    selector: 'app-user-card',
+    templateUrl: './user-card.component.html',
+    styleUrls: ['./user-card.component.scss']
 })
 export class UserCardComponent implements OnInit {
 
-  constructor() { }
+    cardNumber: string;
+    cardHolder: string;
+    expires: string;
+    secretCode: string;
 
-  ngOnInit(): void {
-  }
+    constructor() {
+        this.cardNumber = '**** **** **** 6258';
+        this.cardHolder = 'Artem Kudria';
+        this.expires = '10/25';
+        this.secretCode = '000';
+    }
+
+    ngOnInit(): void {
+    }
 
 }
